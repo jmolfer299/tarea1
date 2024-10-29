a27b9900033e28404df157d565128634925a617d
Jose Maria Molina Fernandez-Crehuet <jmolfer299@g.educaand.com>
2024-10-29 10:06:10 +0100

    Añadimos Par Impar

---
+++ GitEclipse2/src/GitEclipse2/GitParImpar.java
@@ -0,0 +1,29 @@
+package GitEclipse2;
+
+import java.util.Scanner;
+
+public class GitParImpar {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Este programa te pide un número y te dice si es par o impar");
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Introduce un número: ");
+		int numero = Integer.parseInt(scanner.nextLine());
+		
+		scanner.close();
+		int modulo = numero%2;
+		
+		if(modulo != 0)
+		{
+			System.out.println("El número es impar");
+		}
+		else
+		{
+			System.out.println("El número es par");
+		}
+
+	}
+
+}
